bfd1078692d123df9aab442cb8c32b76d82fd988
Md Emon <mdshajdulhaqueemon@gmail.com>
2024-08-18 00:22:07 +0600

    start navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,63 +1,86 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
-
 import { CgMenuGridR } from "react-icons/cg";
+import { GoArrowUpRight } from "react-icons/go";
 const NavBar = () => {
-
-    const navlink=<>
-    
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/course'>Course</NavLink></li>
-        <li><NavLink to='/subscribe'>Subscribe</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/testimoni'>Testimoni</NavLink></li>
-    </>
-
-
     const [activeButton, setActiveButton] = useState(null);
 
     const handleButtonClick = (button) => {
-      setActiveButton(button);
+        setActiveButton(button);
     };
     return (
         <div>
-            <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="text-2xl lg:hidden">
-       <CgMenuGridR/>
-      </div>
-      <ul  tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-       {navlink}
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">Skill<span className="text-[#CB8461]">Shoot</span></a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     {navlink}
-    </ul>
-  </div>
-  <div className="navbar-end  gap-2">
-  <a
-        className={`btn ${activeButton === 'login' ? 'bg-[#CB8461]' : ''}`}
-        onClick={() => handleButtonClick('login')}
-      >
-        Login
-      </a>
-      <a
-        className={`btn ${activeButton === 'register' ? 'bg-[#CB8461]' : ''}`}
-        onClick={() => handleButtonClick('register')}
-      >
-        Register
-      </a>
-  </div>
-</div>
+            <div>
+
+                <nav style={{
+            backgroundImage: "url(https://i.ibb.co/THCMt5k/Rectangle-1.png)",
+          }} className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+                    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+
+                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Slill<span className="text-[#CB8461]">Shoot</span></span>
+                        </a>
+                        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                            <div className="hidden md:block gap-4">
+                                <a
+                                    className={`btn ${activeButton === 'login' ? 'bg-[#CB8461]' : ''}`}
+                                    onClick={() => handleButtonClick('login')}
+                                >
+                                    Login
+                                </a>
+                                <a
+                                    className={`btn ${activeButton === 'register' ? 'bg-[#CB8461]' : ''}`}
+                                    onClick={() => handleButtonClick('register')}
+                                >
+                                    Register
+                                </a>
+                            </div>
+                            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                                <span className="sr-only">Open main menu</span>
+                                <CgMenuGridR className="text-xl"></CgMenuGridR>
+                            </button>
+                        </div>
+                        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                <li>
+                                    <a href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+                                </li>
+                                <li>
+                                    <a href="/course" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Course</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Subscribe</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Testimonial</a>
+                                </li>
+
+                            </ul>
+
+
+                        </div>
+                    </div>
+
+       
+                    <div>
+                        <h1 className="text-3xl">There is always <br /> something new for <br /> us to learn</h1>
+                        <p>we have created more than 100+ materials on various <br /> things that will help your career, with mentors who are <br /> experienced in their fields.</p>
+
+                        <div className="flex">
+                        <button className="btn bg-[#CB8461]">Start Journey <GoArrowUpRight/></button>
+                        <img src="https://i.ibb.co/cFkghkR/Vector-2.png" alt="" />
+                        <img src="https://i.ibb.co/BTr7z06/Vector-3.png" alt="" />
+                        <img src="https://i.ibb.co/BTPbQn4/Vector-5.png" alt="" />
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+
         </div>
     );
 };
-
-
 
 export default NavBar;
